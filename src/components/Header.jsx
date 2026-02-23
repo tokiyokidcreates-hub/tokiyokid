573d16cd67d16547f7e14ecfe0ec5baf9c835a8a
tokiyokidcreates-hub <tokiyokidcreates@gmail.com>
2026-02-23 15:56:08 -0800

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <Link to="/">My Portfolio</Link>
+        </div>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
